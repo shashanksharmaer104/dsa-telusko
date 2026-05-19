cb60f5428bdf0979eed0316686dccdfcc05c17ad
Shashank Sharma <shashanksharma.er104@gmail.com>
2026-05-19 17:03:07 +0530

    Added bubblesort and gitignore files

---
+++ src/test/java/sort/BubbleSort.java
@@ -0,0 +1,39 @@
+package sort;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] nums = {6,5,2,8,9,4};
+        int size = nums.length;
+        int temp = 0;
+
+        System.out.println("Before sorting");
+        for(int num: nums) {
+            System.out.print(num + " ");
+        }
+
+        //logic
+        for(int i=0;i<size; i++) {
+            for (int j=0;j<size-i-1; j++) {
+                if (nums[j] > nums[j+1]) {
+                    temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+
+            System.out.println();
+            for(int num: nums) {
+                System.out.print(num + " ");
+            }
+        }
+
+
+        System.out.println("\nAfter sorting");
+        for(int num: nums) {
+            System.out.print(num + " ");
+        }
+
+    }
+
+}
